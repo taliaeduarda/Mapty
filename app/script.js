e3fb08778b96a4f38fb9c6eb55a34eca5379071d
taliaeduarda <taliaeduardaa@gmail.com>
2021-04-24 21:11:26 -0300

    recomecei

--- app/script.js
+++ app/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -9,199 +8,42 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-class Workout {
-  date = new Date();
-  id = (Date.now() + "").slice(-10);
+// API com a posição do usuário, recebe uma função de sucesso e outra de erro
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-  constructor(coords, distance, duration) {
-    this.coords = coords;
-    this.distance = distance;
-    this.duration = duration;
-  }
+    // Criando uma array com as cordenadas
+    const coords = [latitude, longitude]
 
-  _setDescription() {
-    // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // Inserindo leaflet dentro da função de sucesso
 
-  this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${this.date.getMonth()}`
-  }
-}
-
-class Running extends Workout {
-  type = "running";
-  constructor(coords, distance, duration, cadence) {
-    super(coords, distance, duration);
-    this.cadence = cadence;
-    this.calcPace();
-  }
-
-  calcPace() {
-    // min/km
-    this.pace = this.duration / this.distance;
-    return this.pace;
-  }
-}
-
-class Cycling extends Workout {
-  type = "cycling";
-
-  constructor(coords, distance, duration, elevationGain) {
-    super(coords, distance, duration);
-    this.elevationGain = elevationGain;
-    this.calcSpeed();
-  }
-
-  calcSpeed() {
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
-  }
-}
-
-// const run1 = new Running([39, -12], 5.2, 24, 178)
-// const cicle = new Cycling([39, -12], 27, 95, 523)
-// console.log(run1)
-// console.log(cicle)
-
-//////////////////////////////////////////////////////
-// Aplication Architecture
-class App {
-  // Private instance properties
-  #map;
-  #mapEvent;
-  #workouts = [];
-
-  constructor() {
-    this._getPosition();
-    form.addEventListener("submit", this._newWorkout.bind(this));
-
-    inputType.addEventListener("change", this._toggleElevationField);
-  }
-
-  _getPosition() {
-    if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(
-        this._loadMap.bind(this),
-        function () {
-          alert("Could not get your position");
-        }
-      );
-  }
-
-  _loadMap(position) {
-    const { latitude, longitude } = position.coords;
-    const coords = [latitude, longitude];
-
-    this.#map = L.map("map").setView(coords, 13);
+    const map = L.map("map").setView(coords, 13);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.#map);
+    }).addTo(map);
 
-    // Handling clicks on map
-    this.#map.on("click", this._showForm.bind(this));
-  }
+      // lidando com o clique do usuário no mapa, esse metodo map faz parte do leaflet
+      map.on('click', function(mapEvent) {
+        console.log(mapEvent)
+        const {lat,lng} = mapEvent.latlng
 
-  _showForm(mapE) {
-    this.#mapEvent = mapE;
-    form.classList.remove("hidden");
-    inputDistance.focus();
-  }
-
-  _toggleElevationField() {
-    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-  }
-
-  _newWorkout(e) {
-    const validInputs = (...inputs) =>
-      inputs.every((inp) => Number.isFinite(inp));
-
-    const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
-
-    e.preventDefault();
-
-    // Get data from form
-    const type = inputType.value;
-    const distance = +inputDistance.value; // convert to number
-    const duration = +inputDuration.value;
-    const { lat, lng } = this.#mapEvent.latlng;
-    let workout;
-
-    // If workout running, create running object
-    if (type === "running") {
-      const cadence = +inputCadence.value;
-      // Check if data is valid (not valid in this case)
-      if (
-        !validInputs(distance, duration, cadence) ||
-        !allPositive(distance, duration, cadence)
-      )
-        return alert("Inputs have to be positive numbers!");
-
-      workout = new Running([lat, lng], distance, duration, cadence);
-    }
-
-    // If workout cycling, create running object
-    if (type === "cycling") {
-      const elevation = +inputElevation.value;
-
-      // Check if data is valid
-      if (
-        !validInputs(distance, duration, elevation) ||
-        !allPositive(distance, duration)
-      )
-        return alert("Inputs have to be positive numbers!");
-
-      workout = new Cycling([lat, lng], distance, duration, elevation);
-    }
-    // Add new object to workout array
-    this.#workouts.push(workout);
-    console.log(workout);
-    console.log(this.#workouts);
-
-    // Render workout on map as marker
-    this._renderWorkoutMarker(workout);
-
-    // Render workout on list
-    this._renderWorkout(workout)
-
-    // Hide form + clear input fields
-    inputDistance.value = inputDuration.value = inputDuration.value = inputElevation.value =
-      "";
-  }
-
-  _renderWorkoutMarker(workout) {
-    L.marker(workout.coords)
-      .addTo(this.#map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: `${workout.type}-popup`,
-        })
-      )
-      .setPopupContent("workout")
+        // parte do codigo que cria o marcador 
+        // lat e leng são retornados do objeto que o clique no mapa cria
+      L.marker([lat, lng]) //precisa ser uma array
+      .addTo(map)
+      .bindPopup("Workout")
       .openPopup();
+      })
+  },
+  function () {
+    alert("Não foi possível identificar a sua posição");
   }
+);
 
-  _renderWorkout(workout) {
-    const html = `
-       <li class="workout workout--${workout.type}" data-id="${workout.id}">
-          <h2 class="workout__title">Running on April 14</h2>
-          <div class="workout__details">
-            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
-            <span class="workout__value"${workout.distance}</span>
-            <span class="workout__unit">km</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">⏱</span>
-            <span class="workout__value">${workout.duration}</span>
-            <span class="workout__unit">min</span>
-          </div>
-          `
-  }
-}
-
-const app = new App();
+// Usando uma biblioteca de terceiros
