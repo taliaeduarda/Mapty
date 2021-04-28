d80d083a978fbd64740f0c57eb4109f015171a55
taliaeduarda <taliaeduardaa@gmail.com>
2021-04-28 16:37:05 -0300

    stopped at data management

--- app/script.js
+++ app/script.js
@@ -8,42 +8,96 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// API com a posição do usuário, recebe uma função de sucesso e outra de erro
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+// blueprint
+class App {
+  // private instance properties
+  #map;
+  #mapEvent; // proprs que vão estar presentes em todos os objs criados a partir da cçasse
+
+  constructor() {
+    // constructor é chamado imediatamente assim que um novo objeto for criado a partir dessa classe
+    // garantindo que o metodo vai ser chamado assim que a aplicação carregar
+    this._getPosition();
+
+    form.addEventListener("submit", this._newWorkout.bind(this));
+
+    // lidando com a mudança de opção
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
+
+  _getPosition() {
+    // API com a posição do usuário, recebe uma função de sucesso e outra de erro
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this), // função de sucesso
+
+        function () {
+          alert("Não foi possível identificar a sua posição");
+        }
+      );
+  }
+
+  _loadMap(position) {
     console.log(position);
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     // Criando uma array com as cordenadas
-    const coords = [latitude, longitude]
+    const coords = [latitude, longitude];
 
     // Inserindo leaflet dentro da função de sucesso
 
-    const map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, 13);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-      // lidando com o clique do usuário no mapa, esse metodo map faz parte do leaflet
-      map.on('click', function(mapEvent) {
-        console.log(mapEvent)
-        const {lat,lng} = mapEvent.latlng
-
-        // parte do codigo que cria o marcador 
-        // lat e leng são retornados do objeto que o clique no mapa cria
-      L.marker([lat, lng]) //precisa ser uma array
-      .addTo(map)
-      .bindPopup("Workout")
-      .openPopup();
-      })
-  },
-  function () {
-    alert("Não foi possível identificar a sua posição");
+    // lidando com o clique do usuário no mapa, esse metodo map faz parte do leaflet
+    this.#map.on("click", this._showForm.bind(this));
   }
-);
 
-// Usando uma biblioteca de terceiros
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    // Mostrar o form assim que o user clicar no mapa
+    form.classList.remove("hidden");
+
+    // Automaticamente foca no input distance
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // limpar o valor inserido no campo de input
+    inputDistance.value = inputDuration.value = inputCadence.value = "";
+    // display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    // parte do codigo que cria o marcador
+    // lat e leng são retornados do objeto que o clique no mapa cria
+    L.marker([lat, lng]) //precisa ser uma array
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+  }
+}
+
+// Objeto principal
+const app = new App();
